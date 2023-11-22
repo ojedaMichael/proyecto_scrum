0f1593f5f5a7edf8c2f4e61904afc1309c3aa77f
jonatan <framan2403@gmail.com>
2023-11-22 10:40:38 -0500

    cambio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import Login from "./componentes/Login/login.jsx"
-=======
 import Login from "./componentes/Login/Login.jsx"
-
->>>>>>> f99a519c5b9e2e6cb7f0cbea50e4ca06ff640d2f
 import Dashboard from "./componentes/dashboard/Dashboard.jsx";
 import Empresas from './componentes/empresas/Empresas.jsx';
-import Form from './componentes/prueba.axios/Form.jsx';
+
 import Personas from './componentes/Personas/personas';
 function App() {
 
